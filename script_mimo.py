d79d8d79d46e4f84cc5842d0272340a5d309ab13
tuwadmin <samia.yasmine.l@gmail.com>
2025-12-30 18:57:43 -0500

    step counter added

---
+++ script_mimo.py
@@ -0,0 +1,15 @@
+# Mimo Learning
+# Step Counter
+steps = 0 
+
+monday_steps = 1000
+tuesday_steps = 1000
+wednesday_steps = 1000
+thursday_steps = 1000
+friday_steps = 1000
+saturday_steps = 700
+sunday_steps = 800
+total_steps = monday_steps + tuesday_steps + wednesday_steps + thursday_steps + friday_steps + saturday_steps + sunday_steps
+avg_steps = total_steps/7
+print("Total Steps this Week:", total_steps)
+print("Average Steps this Week:", avg_steps)
