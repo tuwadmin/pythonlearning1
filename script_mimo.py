e87d895bb6a57dbe91bf2033164d34e8d0bec3af
tuwadmin <samia.yasmine.l@gmail.com>
2025-12-30 19:01:41 -0500

    mimo bot build

--- script_mimo.py
+++ script_mimo.py
@@ -1,7 +1,6 @@
 # Mimo Learning
 # Step Counter
 steps = 0 
-
 monday_steps = 1000
 tuesday_steps = 1000
 wednesday_steps = 1000
@@ -13,3 +12,16 @@ total_steps = monday_steps + tuesday_steps + wednesday_steps + thursday_steps + 
 avg_steps = total_steps/7
 print("Total Steps this Week:", total_steps)
 print("Average Steps this Week:", avg_steps)
+print(f"You ran a total of {total_steps} this week!")
+# END OF PREVIOUS PROJECT
+
+# Mimo Project #1 Build a Bot 
+print("Hello! What is your name?")
+name = "Wale"
+print(f"What's your {name}?")
+print("How many albums have you sold?")
+wale_album_sales = 300,000
+bot_album_sales = 3
+album_difference = wale_album_sales - bot_album_sales
+print(f"You are {album_difference} ft taller than me. I've only sold {bot_album_sales} albums total!")
+print("What's your favorite color?")
